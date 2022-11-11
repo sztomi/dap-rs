7417bfdae0a43a4e702d9563c827d69bc7233e94
Tamás Szelei <szelei.t@gmail.com>
2022-11-11 23:24:26 +0100

    examples: expand dummy-server

--- examples/dummy-server/examples/dummy-server.rs
+++ examples/dummy-server/examples/dummy-server.rs
@@ -1,23 +1,41 @@
 use std::fs::File;
-use std::io::{BufWriter, BufReader};
+use std::io::{BufReader, BufWriter};
 
-use dap::{Adapter, BasicClient, Request, Response, Server};
+use dap::responses::ResponseBody::Initialize;
+use dap::types::Capabilities;
+use dap::{Adapter, BasicClient, ClientContext, Command, Request, Response, Server};
 
 use anyhow::Result;
 
 struct MyAdapter;
 
 impl Adapter for MyAdapter {
-  fn accept(&mut self, request: Request) -> Response {
-    println!("accept {:?}", request);
-    Response::make_ack(&request).unwrap()
+  fn accept(&mut self, request: Request, _ctx: &mut dyn ClientContext) -> Response {
+    println!("Accept {:?}\n", request.command);
+
+    match &request.command {
+      Command::Initialize(args) => {
+        println!(
+          "> Client '{}' requested initialization.",
+          args.client_name.as_ref().unwrap()
+        );
+        Response::make_success(&request, Initialize(Some(Capabilities {
+            supports_configuration_done_request: Some(true),
+            supports_evaluate_for_hovers: Some(true),
+            ..Default::default()
+        })))
+      }
+      Command::Next(_) => Response::make_ack(&request).unwrap(),
+      _ => todo!(),
+    }
   }
 }
 
 fn main() -> Result<()> {
-  let adapter = MyAdapter{};
+  let adapter = MyAdapter {};
   let client = BasicClient::new(BufWriter::new(std::io::stdout()));
   let mut server = Server::new(adapter, client);
+
   let f = File::open("testinput.txt")?;
   let mut reader = BufReader::new(f);
 
