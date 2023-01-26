6af6356a946c4a9c6b78af04a4fe1e4a5b03d44e
Eran Ifrah <eran@codelite.org>
2023-01-26 23:47:36 +0200

    added test for FileLineReader

--- src/line_reader.rs
+++ src/line_reader.rs
@@ -30,7 +30,7 @@ impl FileLineReader {
 impl LineReader for FileLineReader {
     /// read an additional `n` bytes from the file and append them to `buffer`
     async fn read_n_bytes(&mut self, buffer: &mut BytesMut, n: usize) -> Result<usize, IoError> {
-        let mut buf = BytesMut::with_capacity(n);
+        let mut buf = vec![0; n];
         self.file.read_exact(&mut buf).await?;
         buffer.extend_from_slice(&buf);
         Ok(n)
@@ -47,5 +47,42 @@ impl LineReader for FileLineReader {
                 return Ok(line);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    const DAP_INIT_REQUEST: &str = r#"Content-Length: 392
+
+{"seq":2,"type":"request","command":"launch","arguments":{"noDebug":false,"program":"C:\\Users\\eran\\Documents\\TestWxCrafter\\build-Debug\\bin\\TestWxCrafter.exe","args":[],"cwd":"C:\\Users\\eran\\Documents\\TestWxCrafter","env":["SHELL=CMD.EXE","CodeLiteDir=C:\\msys64\\home\\eran\\devl\\codelite\\build-release\\install","WXCFG=clang_x64_dll\\mswu","WXWIN=C:\\msys64\\home\\eran\\root"]}}"#;
+
+    use super::*;
+    use tokio::io::AsyncWriteExt;
+    #[tokio::test]
+    async fn test_file_reader() -> Result<(), IoError> {
+        {
+            // preare the input file
+            let mut file = File::create("session.txt").await?;
+            file.write_all(DAP_INIT_REQUEST.as_bytes()).await?;
+            file.flush().await?;
+        }
+
+        tracing_subscriber::fmt::init();
+        let mut reader = FileLineReader::new("session.txt").await;
+        {
+            let line = reader.read_line().await?;
+            assert_eq!(line, "Content-Length: 392\n");
+        }
+        {
+            let line = reader.read_line().await?;
+            assert_eq!(line, "\n");
+        }
+        {
+            // read by length
+            let mut buffer = BytesMut::with_capacity(392);
+            reader.read_n_bytes(&mut buffer, 392).await?;
+            assert_eq!(buffer.len(), 392);
+        }
+        Ok(())
     }
 }
