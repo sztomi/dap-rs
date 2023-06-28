b06189331203ba0a0e945615545cc651fdabe197
Pil Eghoff <pil.eghoff@gmail.com>
2023-06-28 20:18:06 +0200

    Update README and examples to reflect refactor

--- examples/dummy-server.rs
+++ examples/dummy-server.rs
@@ -9,48 +9,38 @@ use dap::prelude::*;
 enum MyAdapterError {
   #[error("Unhandled command")]
   UnhandledCommandError,
-}
 
-struct MyAdapter;
-
-impl Adapter for MyAdapter {
-  type Error = MyAdapterError;
-
-  fn accept(&mut self, request: Request, _ctx: &mut dyn Context) -> Result<Response, Self::Error> {
-    eprintln!("Accept {:?}\n", request.command);
-
-    match &request.command {
-      Command::Initialize(args) => {
-        if let Some(client_name) = args.client_name.as_ref() {
-          eprintln!("> Client '{client_name}' requested initialization.");
-          Ok(Response::make_success(
-            &request,
-            ResponseBody::Initialize(Some(types::Capabilities {
-              supports_configuration_done_request: Some(true),
-              supports_evaluate_for_hovers: Some(true),
-              ..Default::default()
-            })),
-          ))
-        } else {
-          Ok(Response::make_error(&request, "Missing client name"))
-        }
-      },
-      Command::Next(_) => Ok(Response::make_ack(&request).unwrap()),
-      _ => Err(MyAdapterError::UnhandledCommandError),
-    }
-  }
+  #[error("Missing command")]
+  MissingCommandError,
 }
 
 type DynResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> DynResult<()> {
-  let adapter = MyAdapter {};
-  let client = BasicClient::new(BufWriter::new(std::io::stdout()));
-  let mut server = Server::new(adapter, client);
-
+  let output = BufWriter::new(std::io::stdout());
   let f = File::open("testinput.txt")?;
-  let mut reader = BufReader::new(f);
+  let input = BufReader::new(f);
+  let mut server = Server::new(input, output);
 
-  server.run(&mut reader)?;
+  let req = match server.poll_request()? {
+    Some(req) => req,
+    None => return Err(Box::new(MyAdapterError::MissingCommandError)),
+  };
+  if let Command::Initialize(_) = req.command {
+    let rsp = Response::make_success(
+      &req,
+      ResponseBody::Initialize(Some(types::Capabilities {
+        ..Default::default()
+      })),
+    );
+
+    // When you call respond, send_event etc. the message will be wrapped
+    // in a base message with a appropriate seq number, so you don't have to keep track of that yourself
+    server.respond(rsp).unwrap();
+
+    server.send_event(Event::Initialized).unwrap();
+  } else {
+    return Err(Box::new(MyAdapterError::UnhandledCommandError));
+  }
   Ok(())
 }
